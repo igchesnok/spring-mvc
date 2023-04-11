faad480eb1dee2b330e086c9272dee2b2e626e90
igchesnok <igchesnok@gmail.com>
2023-04-11 15:00:55 +0300

    Список машин должен хранится в сервисе или дао и инициализироваться 1 раз.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,6 +1,7 @@
 package web.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,24 +12,20 @@ import web.service.CarService;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Controller
 public class CarController {
 
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService){
+        this.carService = carService;
+    }
+
     @GetMapping("/cars")
     public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
-        //создаем объект для загрузки в модель
-        List<Car> list = new ArrayList<>();
-        list.add(new Car("УАЗ", "КРАCНЫЙ", "1000"));
-        list.add(new Car("Ferrari", "ЖЕЛТЫЙ", "700"));
-        list.add(new Car("ВАЗ", "ЧЕРНЫЙ", "2007"));
-        list.add(new Car("FIAT", "ГОЛУБОЙ", "180"));
-        list.add(new Car("BMW", "СЕРЫЙ", "100"));
-        //редактируем объект с учетом запроса
-        list = CarService.carsCount(list, allCars);
         //передаем обьект list = List<Car> в модель
-        model.addAttribute("list", list);
+        model.addAttribute("list", carService.carsCount( allCars));
         return "cars";
-
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,13 +1,27 @@
 package web.service;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CarService {
-    public static List<Car> carsCount(List<Car> list, int number) {
+
+    //создаем объект для загрузки в модель
+    private  List<Car> list ;
+    public CarService() {
+        list = new ArrayList<>();
+        list.add(new Car("УАЗ", "КРАCНЫЙ", "1000"));
+        list.add(new Car("Ferrari", "ЖЕЛТЫЙ", "700"));
+        list.add(new Car("ВАЗ", "ЧЕРНЫЙ", "2007"));
+        list.add(new Car("FIAT", "ГОЛУБОЙ", "180"));
+        list.add(new Car("BMW", "СЕРЫЙ", "100"));
+    }
+
+    public  List<Car> carsCount(int number) {
         if (number == 0 || number > 5) return list;
         return list.stream().limit(number).collect(Collectors.toList());
     }
